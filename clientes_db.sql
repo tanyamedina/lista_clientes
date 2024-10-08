36cf283fd9e421a2ef0a641f00bc4da343820cbf
Tany <criaturamaldita@gmail.com>
2024-10-08 15:35:56 -0700

    cambio de archivo mysql a postgre

--- clientes_db.sql
+++ clientes_db.sql
@@ -1,7 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 28-09-2024 a las 19:12:41
 -- Versión del servidor: 10.4.28-MariaDB
@@ -11,34 +10,25 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
 -- Base de datos: `clientes_db`
---
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `clientes`
---
 
-CREATE TABLE `clientes` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(35) NOT NULL,
-  `apellido` varchar(35) NOT NULL,
-  `telefono` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE clientes (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(35) NOT NULL,
+  apellido VARCHAR(35) NOT NULL,
+  telefono VARCHAR(15) NOT NULL
+);
 
---
 -- Volcado de datos para la tabla `clientes`
---
 
-INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `telefono`) VALUES
+INSERT INTO clientes (id, nombre, apellido, telefono) VALUES
 (11, 'Fernandita', 'Gomez', '753951852'),
 (12, 'Lucía', 'Fernández', '654789321'),
 (13, 'Carlitos', 'Hernández', '987321654'),
@@ -62,58 +52,17 @@ INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `telefono`) VALUES
 (37, 'Axel', 'Zaldivar', '982912700'),
 (38, 'Rodriguito', 'Amezcua', '278941923');
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `usuarios`
---
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE usuarios (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
---
 -- Volcado de datos para la tabla `usuarios`
---
 
-INSERT INTO `usuarios` (`id`, `username`, `password`) VALUES
+INSERT INTO usuarios (id, username, password) VALUES
 (2, 'admin', '$2y$10$3HRi3h16rzBcN6lAlxPVjuL2HZTwN2UBLM2QK1kM2XCUtzmJBQT9S');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
